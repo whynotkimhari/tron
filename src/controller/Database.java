b49bcd06783b1dbfdd9c904d0bebc96123a19752
wnkh <htlulem185@gmail.com>
2024-12-11 23:26:31 +0100

    Update Database.java

--- src/controller/Database.java
+++ src/controller/Database.java
@@ -19,30 +19,43 @@ import model.Record;
  * @author Nguyen Kim Hai, Bui
  */
 public class Database {
-    private static MysqlConnectionPoolDataSource pooledConn = null;
-    private Database() { }
+    private static Database instance = null;
+    private Connection conn = null;
     
-    public static Connection getConnection() throws SQLException {
-        if (pooledConn == null) {
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                pooledConn = new MysqlConnectionPoolDataSource();
-                pooledConn.setServerName("localhost");
-                pooledConn.setPort(3306);
-                pooledConn.setDatabaseName("Tron");
-                pooledConn.setUser("root");
-                pooledConn.setPassword("wnkh");
-            } catch (ClassNotFoundException e) {
-                System.out.println("No connection Database!");
-            }
+    private Database() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            MysqlConnectionPoolDataSource pool= new MysqlConnectionPoolDataSource();
+            pool.setServerName("localhost");
+            pool.setPort(3306);
+            pool.setDatabaseName("Tron");
+            pool.setUser("root");
+            pool.setPassword("wnkh");
+
+            conn = pool.getPooledConnection().getConnection();
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("No connection Database!");
         }
-        
-        return pooledConn.getPooledConnection().getConnection();
     }
     
-    public static ArrayList<Record> getLeaderboard() {
+    /**
+     * Method to get singleton instance
+     * @return 
+     */
+    public static Database getDatabase() {
+        if (instance == null) {
+            instance = new Database();
+        }
+        
+        return instance;
+    }
+    
+    /**
+     * Get all records
+     * @return 
+     */
+    public ArrayList<Record> getRecords() {
         try {
-            Connection conn = Database.getConnection();
             ArrayList<Record> highScores = new ArrayList<>();
             
             // Execute the query for the first player
@@ -68,10 +81,13 @@ public class Database {
         }
     }
     
-    public static void registerPlayers(String name1, String name2) {
+    /**
+     * Register two players to database
+     * @param name1 player 1's name
+     * @param name2 player 2's name
+     */
+    public void registerPlayers(String name1, String name2) {
         try {
-            Connection conn = Database.getConnection();
-            
             String sql = "INSERT IGNORE INTO player (name, score) VALUES (?, 0), (?, 0);";
 
             // Execute the query for the first player
@@ -88,11 +104,13 @@ public class Database {
         }
     }
     
-    public static int getScore(String name) {
+    /**
+     * Get score of player with given name
+     * @param name player name
+     * @return int
+     */
+    public int getScore(String name) {
         try {
-            Connection conn = Database.getConnection();
-
-            int score;
             // Check if the winner is in database
             try (Statement statement = conn.createStatement()) {
                 // Check if the winner is in database
@@ -100,9 +118,8 @@ public class Database {
                 "SELECT score FROM player WHERE name='%s';"
                     .formatted(name)
                 );  rs.first();
-                score = rs.getInt("score");
+                return rs.getInt("score");
             }
-            return score;
             
         } catch (SQLException z) {
             System.out.println(z);
@@ -111,12 +128,14 @@ public class Database {
         }
     }
     
-    public static void updateScore(String name) {
+    /**
+     * Update score of player with given name
+     * @param name player's name
+     */
+    public void updateScore(String name) {
         int currScore = getScore(name);
         
         try {
-            Connection conn = Database.getConnection();
-
             // Check if the winner is in database=
             try (Statement statement = conn.createStatement()) {
                 // Check if the winner is in database=
